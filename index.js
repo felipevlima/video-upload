38da344c15112a7690abb87eb64d1b9dbfdf1491
Felipe Vieira <felip.3lima@hotmail.com>
2023-09-16 23:41:32 -0300

    Full Settings App

--- index.js
+++ index.js
@@ -1,69 +1,34 @@
 
-const vimeo = require('vimeo').Vimeo
+const Vimeo = require('vimeo').Vimeo
 
 let clientId = "6d0dcf3523fd4d90d347e52dafc6fb2a48e5054a"
 let clientSecret = "LZLu7+5fvdiazGMcsKOHvY3p3mmmsSAMAqNAjzOich0DK/toyl4DFkNT4+6ZN5WTJEpQy7Eju8hfX1irEvW4gNEuPp519BHBa1CAV6mkfNEox4ay8kL5SkeYWV+Bi5uh"
 let accessToken = "5a44d7f76fe08ecf329ee37152d61e56"
 
+const client = new Vimeo(clientId, clientSecret, accessToken)
+
 async function uploadVideo() {
-  const client = new vimeo(clientId, clientSecret, accessToken)
-
-  client.request({
-    method: 'GET',
-    path: '/tutorial'
-  }, function (error, body, status_code, headers) {
-    if (error) {
-      console.log(error);
+  let file_name = "./example.mp4"
+  client.upload(
+  file_name,
+  {
+    'name': 'Untitled23',
+    'description': 'The description goes here.',
+    'privacy': {
+      "view": 'nobody'
     }
-
-    console.log(body);
-  })
+  },
+  function (uri) {
+    console.log('Your video URI is: ' + uri);
+  },
+  function (bytes_uploaded, bytes_total) {
+    var percentage = (bytes_uploaded / bytes_total * 100).toFixed(2)
+    console.log(bytes_uploaded, bytes_total, percentage + '%')
+  },
+  function (error) {
+    console.log('Failed because: ' + error)
+  }
+)
 }
 
 uploadVideo()
-// const jwtClient = new google.auth.OAuth2(
-//   serviceAccount.client_email,
-//   null,
-//   serviceAccount.private_key,
-//   ['https://www.googleapis.com/auth/youtube.upload']
-// );
-
-// jwtClient.authorize((err) => {
-//   if (err) {
-//     console.error('Authorization error:', err);
-//     return;
-//   }
-
-//   // Now, you can use jwtClient to upload a video
-//   const youtube = google.youtube('v3');
-//   const videoPath = './example.mp4'; // Update with your video path
-//   const videoMetadata = {
-//     snippet: {
-//       title: 'Your Video Title',
-//       description: 'Your Video Description',
-//       tags: ['tag1', 'tag2'], // Optional: Add tags
-//       categoryId: '22', // Optional: Set the video category (Entertainment in this case)
-//     },
-//     status: {
-//       privacyStatus: 'private', // Set video privacy status (private, public, unlisted)
-//     },
-//   };
-
-//   youtube.videos.insert(
-//     {
-//       auth: jwtClient,
-//       part: 'snippet,status',
-//       resource: videoMetadata,
-//       media: {
-//         body: fs.createReadStream(videoPath),
-//       },
-//     },
-//     (err, response) => {
-//       if (err) {
-//         console.error('Error uploading video:', err);
-//         return;
-//       }
-//       console.log('Video uploaded successfully. Video ID:', response.data.id);
-//     }
-//   );
-// });
